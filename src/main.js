76fa46257ccfff0c6d70f6b00f95a8672f1fbf2d
delorescetleh <71180656+delorescetleh@users.noreply.github.com>
2020-09-14 18:01:44 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue';
+import VeLine from 'v-charts/lib/line.common';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+Vue.component(VeLine.name, VeLine);
 
 Vue.config.productionTip = false;
 
